80de4fbe3a6347b5b93d7fac84fbf41f16f73f0b
Gautam Mishra <njoygautam12@gmail.com>
2024-05-08 01:32:37 +0530

    files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import { Container, Grid } from '@mui/material';
+import Profile from './component/Profile/Profile';
+import Header from './component/Header/Header';
+import Portfolio from './Pages/Portfolio/Portfolio';
+import Resume from './Pages/Resume/Resume';
+import Footer from './component/Footer/Footer'; 
 
-function App() {
+import { BrowserRouter as Router,Switch,Route } from 'react-router-dom';
+
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Container>
+    <Grid container>
+      <Grid item xs={12} sm={12} md={4} lg={3}>
+        <Profile />
+      </Grid>
+      <Grid item xs>
+        <Header />
+
+        <Router>
+        <Switch>
+          <Route path="/portfolio">
+              <Portfolio />
+          </Route>
+          <Route path="/">
+              <Resume />
+          </Route>
+        </Switch>
+        </Router>
+
+        <Footer />
+      </Grid>
+    </Grid>
+   </Container>
   );
 }
 
